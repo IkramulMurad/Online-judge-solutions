0e5b733d70cba671b6295ccfc7977f9cd3ad3052
Ikramul Murad <Murad1993@users.noreply.github.com>
2016-12-23 08:01:08 +0600

    272 - TEX Quotes

---
+++ UVA/272 - TEX Quotes.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    //freopen("in.txt","r",stdin);
+    //freopen("out.txt","w",stdout);
+    char c;
+    string s;
+    int flag=0;
+    while(~scanf("%c",&c)){
+        if(c=='"' and !flag){
+            s+="``";
+            flag=1;
+        }
+        else if(c=='"' and flag){
+            s+="''";
+            flag=0;
+        }
+        else{
+            s.push_back(c);
+        }
+    }
+    cout<<s;
+    return 0;
+}
