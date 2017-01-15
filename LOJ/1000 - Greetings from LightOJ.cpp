1d23b362ab7ce15350dfe717d946bff82492a388
Ikramul Murad <Murad1993@users.noreply.github.com>
2017-01-15 19:05:34 +0600

    Update 1000 - Greetings from LightOJ.cpp

--- LOJ/1000 - Greetings from LightOJ.cpp
+++ LOJ/1000 - Greetings from LightOJ.cpp
@@ -1,13 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main (){
-int t;
-cin>>t;
-for (int i=1;i<=t;i++){
-int a,b,c;
-cin>>a>>b;
-c=a+b;
-cout<<"Case "<<i<<": "<<c<<endl;
-}
-return 0;
+int main ()
+{
+    int t;
+    cin>>t;
+    for (int i=1; i<=t; i++)
+    {
+        int a,b,c;
+        cin>>a>>b;
+        c=a+b;
+        cout<<"Case "<<i<<": "<<c<<endl;
+    }
+    return 0;
 }
