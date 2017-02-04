bfc72ec8fc6f960ede5b9d631249777ab33b229f
Ikramul Murad <Murad1993@users.noreply.github.com>
2017-02-05 01:16:43 +0600

    11151 - Longest Palindrome

--- UVA/11151 - Longest Palindrome.cpp
+++ UVA/11151 - Longest Palindrome.cpp
@@ -1,3 +1,5 @@
+//recursive dp
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -29,6 +31,56 @@ int main()
         getline(cin,s);
         memset(dp,-1,sizeof dp);
         cout<<lps(0,s.size()-1)<<endl;
+    }
+
+    return 0;
+}
+
+//iterative dp
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dp[1004][1004];
+string s;
+
+void solve(){
+    int n=s.size();
+    if(n==0){
+        cout<<0<<endl;
+        return;
+    }
+
+    for(int i=0; i<n; ++i) dp[i][i]=1;
+
+    for(int l=2; l<=n; ++l){
+        for(int i=0; i<=n-l; ++i){
+            int j=l-1+i;
+            if(s[i]==s[j] and i+1==j){
+                dp[i][j]=2;
+            }
+            else if(s[i]==s[j]){
+                dp[i][j]=2+dp[i+1][j-1];
+            }
+            else{
+                dp[i][j]=max(dp[i][j-1],dp[i+1][j]);
+            }
+        }
+    }
+
+    cout<<dp[0][n-1]<<endl;
+}
+
+int main()
+{
+    int tc;
+    cin>>tc;
+    getchar();
+
+    for(int t=1; t<=tc; ++t){
+        getline(cin,s);
+        memset(dp,0,sizeof dp);
+        solve();
     }
 
     return 0;
