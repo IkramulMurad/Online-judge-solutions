0c3a4541afe18c0d7a4e58657cebe6503ede3007
Ikramul Murad <IkramulMurad@users.noreply.github.com>
2018-03-22 23:25:43 +0600

    1264. Workdays

---
+++ Timus/1264. Workdays.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long res;
+    int n,m;
+    while(cin>>n>>m){
+        res=n*(m+1);
+        cout<<res<<endl;
+    }
+    return 0;
+}
