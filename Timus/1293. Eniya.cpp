cb8a577bdcb08f4d60850225b4447bfb41a0d70a
Ikramul Murad <IkramulMurad@users.noreply.github.com>
2018-03-22 23:24:17 +0600

    1293. Eniya

---
+++ Timus/1293. Eniya.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,a,b;
+    while(cin>>n>>a>>b){
+        long long res=2*n*a*b;
+        cout<<res<<endl;
+    }
+    return 0;
+}
