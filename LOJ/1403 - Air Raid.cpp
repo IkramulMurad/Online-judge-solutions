b1ba67e56c4e7ab366458aa782a18cb5e257c66b
Ikramul Murad <IkramulMurad@users.noreply.github.com>
2018-09-18 10:04:45 +0600

    1403 - Air Raid

---
+++ LOJ/1403 - Air Raid.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mx = 1005;
+vector <vector <int> > g(mx);
+int l[mx], r[mx];
+bool visited[mx];
+int color[mx];
+
+bool dfs(int u){
+    for(int i = 0; i < g[u].size(); ++i){
+        int v = g[u][i];
+        if(!visited[v]){
+            visited[v] = true;
+            if(l[v] == -1 or dfs(l[v])){
+                l[v] = u;
+                r[u] = v;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int bpm(int n){
+    int total = 0;
+    memset(l, -1, sizeof(l));
+    memset(r, -1, sizeof(r));
+
+    for(int i = 1; i <= n; ++i){
+        memset(visited, false, sizeof(visited));
+        if(dfs(i)) ++total;
+    }
+    return total;
+}
+
+int main(){
+    //freopen("in.txt", "r", stdin);
+
+    int tc;
+    scanf("%d", &tc);
+
+    for(int t = 1; t <= tc; ++t){
+        int n, m;
+        scanf("%d %d", &n, &m);
+
+        for(int i = 0; i < mx; ++i) g[i].clear();
+
+        for(int i = 0; i < m; ++i){
+            int u, v;
+            scanf("%d %d", &u, &v);
+
+            g[u].push_back(v);
+        }
+
+        int ans = n - bpm(n);
+        printf("Case %d: %d\n", t, ans);
+    }
+
+    return 0;
+}
